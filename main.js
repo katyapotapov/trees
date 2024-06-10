72e0f022e9c1a2361c836394bb866e9b57edad79
Katya Potapov <kat.p28@gmail.com>
2024-06-10 02:20:52 -0400

    Messed up parser probably doesn't work because the cylinder positioning is wrong

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.body.appendChild(renderer.domElement);
 // Now we initialize the L-system with the axiom
 const axiom = "F";
 const rules = { F: "F[-F][+F]" };
-const angle = 25;
+const angle = 90;
 
 function lSystemForN(axiom, rules, n) {
   let curState = axiom;
@@ -39,27 +39,55 @@ function lSystemForN(axiom, rules, n) {
   return curState;
 }
 
-console.log(lSystemForN(axiom, rules, 5));
+const lSystem = lSystemForN(axiom, rules, 1);
+console.log(lSystem);
 
-function addCylinder(posX, posY, rot) {
+function addCylinder(posX, posY, posZ, rot) {
   const geometry = new THREE.CylinderGeometry(1, 1, 20, 32);
 
   geometry.translate(0, 10, 0);
   const material = new THREE.MeshBasicMaterial({ color: 0x382921 });
   const cylinder = new THREE.Mesh(geometry, material);
   cylinder.rotateZ(THREE.MathUtils.degToRad(rot));
-  cylinder.position.set(posX, posY, 0);
+  cylinder.position.set(posX, posY, posZ);
 
   return cylinder;
 }
 
-camera.position.z = 50;
+camera.position.z = 500;
 camera.position.y = 20;
 
 const tree = new THREE.Group();
 
-tree.add(addCylinder(0, 0, 0));
-tree.add(addCylinder(0, 10, 20));
+function parseLSystem(lSystem, angle) {
+  let posStack = [[0, 0, 0]];
+  let angleStack = [0];
+  for (let i = 0; i < lSystem.length; i++) {
+    if (lSystem[i] == "F") {
+      const curPos = posStack[posStack.length - 1];
+      const curAngle = angleStack[angleStack.length - 1];
+      tree.add(addCylinder(curPos[0], curPos[1], curPos[2], curAngle));
+      curPos[0] += Math.sin(angle) * 20;
+      curPos[1] += Math.cos(angle) * 20;
+      posStack[posStack.length - 1] = curPos;
+    } else if (lSystem[i] == "-") {
+      angleStack[angleStack.length - 1] -= angle;
+    } else if (lSystem[i] == "+") {
+      angleStack[angleStack.length - 1] += angle;
+    } else if (lSystem[i] == "[") {
+      posStack.push(posStack[posStack.length - 1]);
+      angleStack.push(angleStack[angleStack.length - 1]);
+    } else if (lSystem[i] == "]") {
+      posStack.pop();
+      angleStack.pop();
+    }
+  }
+}
+
+parseLSystem(lSystem, angle);
+
+// tree.add(addCylinder(0, 0, 0));
+// tree.add(addCylinder(0, 10, 20));
 scene.add(tree);
 
 const geometry = new THREE.PlaneGeometry(10000, 10000);
@@ -69,9 +97,10 @@ const material = new THREE.MeshBasicMaterial({
   side: THREE.DoubleSide,
 });
 const ground = new THREE.Mesh(geometry, material);
-scene.add(ground);
+// scene.add(ground);
 
 function animate() {
+  // tree.rotateY(0.005);
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
